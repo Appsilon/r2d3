b4ab3314d9675fe6a41d0c5cd1f339115ed669a5
Krzysztof Wrobel <krzysztofwrobel@Krzysztofs-MacBook-Air.local>
2017-02-09 19:01:55 +0100

    PDF generation fixes

--- proposal/ghgenerate.R
+++ proposal/ghgenerate.R
@@ -1,4 +1,4 @@
 proposal.file<-"isc-proposal.Rmd"
 
 rmarkdown::render(proposal.file, output_format="html_document", output_dir="../", output_file='index.html', quiet=TRUE)
-rmarkdown::render(proposal.file, output_format="pdf_document", output_dir="out", quiet=TRUE)
+rmarkdown::render(proposal.file, output_dir="out", quiet=TRUE)
