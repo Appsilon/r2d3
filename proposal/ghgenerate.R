c2a21359677c96674169931411a522dab57b7d7d
Filip Stachura <filip@appsilon.pl>
2017-02-09 13:11:44 +0100

    HTML proposal ready.

--- proposal/ghgenerate.R
+++ proposal/ghgenerate.R
@@ -2,7 +2,5 @@ proposal<-"r2d3 - R domain specific language for D3.js"
 proposal.file<-"isc-proposal.Rmd"
 author<-"Filip Stachura, Marek Rogala, Olga Mierzwa-Sulima,  Krzysztof Wróbel on behalf of Appsilon Data Science company. Contact email: hello@appsilondatascience.com; contact number: +48 509 125 362"
 
-rmarkdown::render(proposal.file, output_format="html_document",
-                  output_dir="out", quiet=TRUE)
-rmarkdown::render(proposal.file, output_format="pdf_document",
-                  output_dir="out", quiet=TRUE)
+rmarkdown::render(proposal.file, output_format="html_document", output_dir="../", output_file='index.html', quiet=TRUE)
+rmarkdown::render(proposal.file, output_format="pdf_document", output_dir="out", quiet=TRUE)
