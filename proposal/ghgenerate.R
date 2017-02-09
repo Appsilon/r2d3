48f4b8547a0a90e6dd01a25d2659f98106b1cf3d
Krzysztof Wrobel <krzysztofwrobel@Krzysztofs-MacBook-Air.local>
2017-02-09 11:57:54 +0100

    Proposal template fixes

--- proposal/ghgenerate.R
+++ proposal/ghgenerate.R
@@ -1,6 +1,6 @@
 proposal<-"r2d3 - R domain specific language for D3.js"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Filip Stachura, Marek Rogala, Olga Mierzwa-Sulima,  Krzysztof Wróbel"
+author<-"Filip Stachura, Marek Rogala, Olga Mierzwa-Sulima,  Krzysztof Wróbel on behalf of Appsilon Data Science company. Contact email: hello@appsilondatascience.com; contact number: +48 509 125 362"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
