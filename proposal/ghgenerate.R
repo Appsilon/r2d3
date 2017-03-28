fd9b5400abceb74c2664238a212ce5ec350c276c
Filip Stachura <filip@appsilon.pl>
2017-03-28 18:57:08 +0200

    Proposal removed.

--- proposal/ghgenerate.R
+++
@@ -1,4 +0,0 @@
-proposal.file<-"isc-proposal.Rmd"
-
-rmarkdown::render(proposal.file, output_format="html_document", output_dir="../", output_file='index.html', quiet=TRUE)
-rmarkdown::render(proposal.file, output_dir="out", quiet=TRUE)
